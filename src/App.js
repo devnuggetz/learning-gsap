9376ec5bd9416380f564734ba0089e54b9e1f1e1
shuvamk <kshuvam564@gmail.com>
2020-11-02 01:41:02 -0800

    Initialized intial animation for the circles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import { useEffect, useRef } from 'react';
 
 
 function App() {
+  let circle= useRef(null);
+  let circleRed= useRef(null);
+  let circleBlue= useRef(null);
+  useEffect(()=>{
+    TweenMax.from(circle, 0.8, {opacity:0, x:40, ease: Power3.easeOut})
+    TweenMax.from(circleBlue, 0.8, {opacity:0, x:40, ease: Power3.easeOut, delay:.4})
+    TweenMax.from(circleRed, 0.8, {opacity:0, x:40, ease: Power3.easeOut, delay: .2})
+
+  },[])
+
   return (
 
     <div className="App">
       <header className="App-header">
         <div className='circle-container'>
-          <div className='circle'>
+          <div 
+          ref={el=>{
+            circle=el;
+          }}
+          className='circle'>
 
           </div>
-          <div className='circle red'>
+          <div 
+          ref={el=>{
+            circleRed=el;
+          }}
+          className='circle red'>
 
           </div>
-          <div className='circle blue'>
+          <div 
+          ref={el=>{
+            circleBlue=el;
+          }}
+          className='circle blue'>
 
           </div>
         </div>
