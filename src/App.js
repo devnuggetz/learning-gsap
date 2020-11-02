97818657011dab87b9456cb8cb8a2b43519f1faf
shuvamk <kshuvam564@gmail.com>
2020-11-02 01:43:52 -0800

    Fixed the white flicker effect in animation

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import { useEffect, useRef } from 'react';
 
 
 function App() {
+  let app = useRef(null)
   let circle= useRef(null);
   let circleRed= useRef(null);
   let circleBlue= useRef(null);
   useEffect(()=>{
+     TweenMax.to(app,0,{css: {visibility: 'visible'}})
     TweenMax.from(circle, 0.8, {opacity:0, x:40, ease: Power3.easeOut})
     TweenMax.from(circleBlue, 0.8, {opacity:0, x:40, ease: Power3.easeOut, delay:.4})
     TweenMax.from(circleRed, 0.8, {opacity:0, x:40, ease: Power3.easeOut, delay: .2})
-
   },[])
 
   return (
 
-    <div className="App">
+    <div 
+    ref={el=>{
+      app=el
+    }}
+    className="App">
       <header className="App-header">
         <div className='circle-container'>
           <div 
